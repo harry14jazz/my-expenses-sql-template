73ddbda7a9eaa8888869cbb41f4f69e63b699e2b
harry kurniansyah <32659354+harry14jazz@users.noreply.github.com>
2021-05-05 14:59:37 +0700

    first commit

---
+++ myexpenses.sql
@@ -0,0 +1,40 @@
+-- My Expenses Database
+
+-- CREATE DATABASE
+CREATE DATABASE IF NOT EXISTS my_expenses;
+
+USE my_expenses;
+
+-- CREATE Category Table
+CREATE TABLE ex_category(
+	category_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (category_id)
+);
+
+-- source: https://localfirstbank.com/article/budgeting-101-personal-budget-categories/
+-- INSERT INTO Table
+INSERT INTO `my_expenses`.`ex_category` (`category_id`, `name`) VALUES ('1', 'Housing');
+INSERT INTO `my_expenses`.`ex_category` (`category_id`, `name`) VALUES ('2', 'Transportation');
+INSERT INTO `my_expenses`.`ex_category` (`category_id`, `name`) VALUES ('3', 'Food');
+INSERT INTO `my_expenses`.`ex_category` (`category_id`, `name`) VALUES ('4', 'Utilities');
+INSERT INTO `my_expenses`.`ex_category` (`category_id`, `name`) VALUES ('5', 'Clothing');
+INSERT INTO `my_expenses`.`ex_category` (`category_id`, `name`) VALUES ('6', 'Medical');
+INSERT INTO `my_expenses`.`ex_category` (`category_id`, `name`) VALUES ('7', 'Household Items');
+INSERT INTO `my_expenses`.`ex_category` (`category_id`, `name`) VALUES ('8', 'Personal');
+INSERT INTO `my_expenses`.`ex_category` (`category_id`, `name`) VALUES ('9', 'Debt');
+INSERT INTO `my_expenses`.`ex_category` (`category_id`, `name`) VALUES ('10', 'Savings');
+INSERT INTO `my_expenses`.`ex_category` (`category_id`, `name`) VALUES ('11', 'Gifts');
+INSERT INTO `my_expenses`.`ex_category` (`category_id`, `name`) VALUES ('12', 'Entertaiment');
+
+-- CREATE Expenses Table
+CREATE TABLE expanses(
+	id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    price INT NOT NULL,
+    id_category INT,
+    date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+	CONSTRAINT FK_CategoryExpenses FOREIGN KEY (id_category) 
+    REFERENCES ex_category(category_id)
+);
